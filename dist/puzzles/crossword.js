e84c13930631efe718e0cc5579e41384f853a09f
Wesley Wigham <wwigham@gmail.com>
2017-04-06 22:05:18 -0400

    I AM AN IDIOT

--- dist/puzzles/crossword.js
+++ dist/puzzles/crossword.js
@@ -9,7 +9,7 @@ class BasicDictionary {
         this.storedSearches = {};
         this.storedResults = {};
         this.cachedKeys = [];
-        dict = dict.replace(/[^a-zA-Z0-9]/igm, ""); // Prune all punctuation?
+        this.dict = dict.replace(/[^a-zA-Z0-9]/igm, ""); // Prune all punctuation?
     }
     get(pattern) {
         return this.storedSearches[pattern] || (this.storedSearches[pattern] = new RegExp(`\\b${pattern.replace(/\?/g, ".")}\\b`, "ig"));
